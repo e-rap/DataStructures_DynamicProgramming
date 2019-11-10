fbfe86d9f7ceb10d9047c41d05bab2eb44c07535
e-rap <sir.spaniard@gmail.com>
2019-11-10 18:45:30 -0500

    added comments and cleaned up code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include <iostream>      // for std::cin, std::cout
+#include <functional>    // for std::function
 #include <vector>
 #include <unordered_map>
-#include <functional>
-#include <numeric>
 
 using price = unsigned int;
 using rod_prices = std::vector<price>;
 using test_cases = std::vector<rod_prices>;
 
+/// test case input processing function
+/// @returns a vector containing test case data
 test_cases process_inputs()
 {
   size_t number_of_tests{};
@@ -29,15 +30,18 @@ test_cases process_inputs()
   return tests;
 }
 
+/// calculates the max price for cutting a rod based on given price per length
+///
+/// @param rod_length the length of the rod
+/// @param prices a vector of prices for each length
+/// @returns the max price possible for a given rod length
 price rod_cutting(const price rod_length, const rod_prices& prices)
 {
   std::unordered_map<price, price> answers{};
   answers[1] = prices[0]; // base case, rod of length 1
-  std::function<price(const price)> f{ [&f,&prices, &answers](const price rod_length)
+  std::function<price(const price)> f{ [&f, &prices, &answers](const price rod_length)
   {
-    price max_price{prices[rod_length - 1]};
-
-    // check if answer exists
+    price max_price{}; // price with no cut
     if (answers.find(rod_length) != answers.cend())
     {
       max_price = answers.at(rod_length);
@@ -45,10 +49,9 @@ price rod_cutting(const price rod_length, const rod_prices& prices)
     else // calculate the maximum price
     {
       max_price = prices[rod_length - 1]; // no cut
-      for (size_t digit{ 1 }; digit <= (rod_length / 2); ++digit)
+      for (size_t length{ 1 }; length <= (rod_length / 2); ++length)
       {
-        size_t other = (rod_length) - digit;
-        price price = f(digit) + f(other);
+        price price = f(length) + f(rod_length - length);
         if (price > max_price)
         {
           max_price = price;
